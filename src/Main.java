613fa3be33cdb3bac77f54792250dd727fe60209
Bunny70pl <bunny70pl@gmail.com>
2024-09-04 13:42:10 +0200

    losowanie liczby calkowitej

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("hello world");
         System.out.println("Witaj na lekcji");
 
-        double wylosowana_liczba = Math.random()*10+1;
+        int wylosowana_liczba = (int)(Math.random()*10+1);
         System.out.println(wylosowana_liczba);
     }
 }
