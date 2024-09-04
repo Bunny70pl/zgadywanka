e266cf011647d17136e41139ab08050669a0e28d
Bunny70pl <bunny70pl@gmail.com>
2024-09-04 13:40:21 +0200

    losowanie liczby rzeczywistej

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        //,git init ,git config user.name user.email ,git commit -m "",git remote add origin ,git push -u master
         System.out.println("hello world");
         System.out.println("Witaj na lekcji");
-        //,git init ,git config user.name user.email ,git commit -m "",git remote add origin ,git push -u master
+
+        double wylosowana_liczba = Math.random()*10+1;
+        System.out.println(wylosowana_liczba);
     }
 }
