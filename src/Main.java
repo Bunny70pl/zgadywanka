2a807bfc3985fcd54da7e388b672b67bc2d9f36c
Bunny70pl <bunny70pl@gmail.com>
2024-09-04 13:33:27 +0200

    komendy git

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("Witaj na lekcji");
+        //,git init ,git config user.name user.email ,git commit -m "",git remote add origin ,git push -u master
     }
 }
