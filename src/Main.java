acd66c321f98a10e7366936f3b537e1ad6a99cc5
Bunny70pl <bunny70pl@gmail.com>
2024-09-05 07:28:14 +0200

    sprawdzanie

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        //,git init ,git config user.name user.email ,git commit -m "",git remote add origin ,git push -u master
+        //,git init ,git config user.name user.email,git add . ,git commit -m "",git remote add origin ,git push -u master
         System.out.println("hello world");
         System.out.println("Witaj na lekcji");
 
